0346bd9edb5a8553a1cb8b3119a123adc13c2ae3
mh0316 <85312998+mh0316@users.noreply.github.com>
2022-08-21 20:47:24 -0400

    Add files via upload

---
+++ Blackjack.java
@@ -0,0 +1,158 @@
+import java.sql.Array;
+import java.util.List;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.*;
+
+public class Blackjack {
+    public static void main(String[] args) {
+        crearBaraja();
+    }
+
+    public static String[][] crearBaraja(){
+        var pintas = new String[]{"Corazón","Diamante","Trébol","Pica"};
+        var numerosCartas = new String[]{"Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho","Nueve","Diez","As","Queen","King","Kaiser"};
+        return new String[][]{pintas, numerosCartas};
+    }
+
+    public static void barajar(){
+        String[][] baraja = new String[0][0];
+
+        for(int i = 0; i < baraja.length; i++){
+            Random ronda = new Random();
+            for(int o = baraja[i].length - 1; o > 0; o++){
+                int index = ronda.nextInt(o + 1);
+                String a = baraja[i][index];
+                baraja[i][index] = baraja[i][o];
+                baraja[i][o] = a;
+            }
+        }
+    }
+
+    public static void repartir(String[][] baraja, String [] mano){
+        var carta = new String[2];
+        for (var j = 0; j < mano.length; j++) {
+            for (var i = 0; i < baraja.length; i++) {
+                carta[i] = baraja[i][(int) (Math.random() * baraja[i].length)];
+            }
+            mano[j] = carta[0] + " " + carta[1];
+        }
+    }
+
+    public static HashMap<String, Integer> crearMapa(){
+        var cartas = List.of("AS","DOS","TRES","CUATRO","CUATRO","CINCO","SEIS","SIETE","OCHO","NUEVE","DIEZ","JOTA","QUINA","KAISER");
+        var mapa = new HashMap<String, Integer>();
+        var valorCarta = 0;
+
+        for(var carta : cartas){
+            if(valorCarta > 10){
+                valorCarta = 10;
+            }
+            mapa.put(carta, valorCarta);
+            valorCarta++;
+        }
+
+        return mapa;
+    }
+
+    public static List<Integer> obtenerValoresCartas(){
+        List<String> mano = null;
+
+        var valoresCartas = new ArrayList<Integer>();
+        var mapa = crearMapa();
+        for(var carta : mano){
+            valoresCartas.add(mapa.get(carta));
+        }
+
+        return valoresCartas;
+    }
+
+    public static void bajarse(String[] manoJugador, String[][] manoDealer){
+
+    }
+
+    public static int obtenerValorDeMano(List<String> mano){
+        int valorTotal = 0;
+        for(String carta : mano){
+            valorTotal += obtenerValorDeMano(Collections.singletonList(carta));
+        }
+
+        return valorTotal;
+    }
+
+    public static boolean sePasoDe21(){
+        String mano = String.valueOf(0);
+
+        if(obtenerValorDeMano(Collections.singletonList((mano))) > 21){
+            return  true;
+        }else{
+            return false;
+        }
+    }
+
+    public int obtenerValorDeCarta(String carta) {
+        String valorCarta = carta.split(" ")[1];
+
+        switch (valorCarta) {
+            case "AS":
+                return 1;
+            case "DOS":
+                return 2;
+            case "TRES":
+                return 3;
+            case "CUATRO":
+                return 4;
+            case "CINCO":
+                return 5;
+            case "SEIS":
+                return 6;
+            case "SIETE":
+                return 7;
+            case "OCHO":
+                return 8;
+            case "NUEVE":
+                return 9;
+            case "DIEZ":
+                return 10;
+            case "JOTA":
+                return 10;
+            case "QUINA":
+                return 10;
+            case "KAISER":
+                return 10;
+            default:
+                return 0;
+        }
+    }
+
+    public static void esBlackjack(){
+
+    }
+
+    public static boolean verificarGanador(String[] manoJugador, String[] manoDealer){
+        String[] ganador;
+    }
+
+    public static String[] crearMano(){
+        String[] mano = new String[2];
+        return mano;
+    }
+
+    public static void jugar(){
+        repartir();
+        bajarse();
+        sePasoDe21();
+        esBlackjack();
+        verificarGanador();
+        crearMano();
+        barajar();
+        obtenerValoresCartas();
+        crearMapa();
+        jugar();
+        mostrarMenu();
+    }
+
+    public static void mostrarMenu(){
+
+    }
+}
